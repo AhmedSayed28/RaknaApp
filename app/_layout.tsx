736c5d28a7a7db632bf37f7eb7ba10a3edf78586
Ahmed Sayed <a.ahmed@orobasoft.com>
2026-01-10 07:03:01 +0200

    fix import path for UserProvider in RootLayout; ensure consistency in module resolution

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
-import { UserProvider } from '@/src/context/UserRoleContext';
 import { Stack } from 'expo-router';
+import { UserProvider } from '../src/context/UserRoleContext'; // بنستورد المظلة
 
 export default function RootLayout() {
   return (
     <UserProvider>
+      
       <Stack screenOptions={{ headerShown: false }}>
+        
         <Stack.Screen name="index" /> 
+        
+        <Stack.Screen name="(driver)" />
+        
+        <Stack.Screen name="(provider)" />
+        
       </Stack>
+
     </UserProvider>
   );
 }
